20d8f61b7aa822eb2b8d94d3e5d8d4c30eee4c3d
Balamiayush~ <aryanbalami54@gmail.com>
2026-02-04 15:59:08 +0545

    starter templete

--- src/shared/components/layouts/footer/Footer.jsx
+++ src/shared/components/layouts/footer/Footer.jsx
@@ -1,19 +1,7 @@
 "use client";
-
-import Image from "next/image";
-
-import LayoutWrapper from "../wrapper/LayoutWrapper";
-import { FooterDesktop, FooterTablet, FooterMobile } from "./";
-
-import { useScreenDetector } from "@/shared/hooks/use-screen-detector";
-
 export default function Footer() {
-  const width = useScreenDetector();
 
-  const date = new Date();
-  const year = date.getFullYear();
 
-  const videoSrc = "https://cdn.pixabay.com/video/2025/12/21/323513_large.mp4";
   return (
     <footer className="">
     
